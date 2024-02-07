20025e518b1c04f8f06e12750a11d9cfa2a699ff
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-02-08 00:01:57 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -94,8 +94,8 @@ async def stats(_, message):
 
 async def start(client, message):
     buttons = ButtonMaker()
-    buttons.ubutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.ubutton("Owner", "https://t.me/anas_tayyar")
+    buttons.ubutton("Update Channel", "https://t.me/Hpbot_update")
+    buttons.ubutton("Owner", "https://t.me/Mrkiller_1109")
     reply_markup = buttons.build_menu(2)
     if await CustomFilters.authorized(client, message):
         start_string = f"""
